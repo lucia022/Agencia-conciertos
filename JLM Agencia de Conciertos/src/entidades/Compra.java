f83acfaf6c95f48396b596513091bfab70fd0262
usuario <usuario@192.168.1.132>
2021-01-12 23:30:41 +0100

    He terminado de crear todos los metodos basicos de las clases que me tocaban asi como he tenido que crear una nueva clase llamada Fecha puesto que me estaba dando errores los distintos import de fechas como sin el util y el sql,por lo que he decidido cambiar de sitio la funcion dameFecha.

--- JLM Agencia de Conciertos/src/entidades/Compra.java
+++ JLM Agencia de Conciertos/src/entidades/Compra.java
@@ -5,8 +5,6 @@
  */
 package entidades;
 
-import java.text.ParseException;
-import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -15,8 +13,16 @@ import java.util.Scanner;
  */
 public class Compra {
 
+    //Todos los atributos no podran quedar vacios
+    //VALORES VALIDOS: Long mayores que 0.
+    //VALORES INVALIDOS: Todo lo que no sea long y que el long sea menor igual que 0.
     private long id;
+    //VALORES VALIDOS: Todos aquellos double que sean mayores que 0.01.
+    //VALORES INVALIDOS: Todo aquello que no sea double o los doubles menores que cero.
     private double precio;
+    //VALORES VALIDOS: Los metodos de pago solo podran ser los dados por la consola y habra que elegir entre ellos,en este caso son cadena de caracteres(String).
+    //VALORES INVALIDOS: Todo aquello que no sean String.
+   
     private String metodopago;
     private Usuario idusuario;
     /*Relación "realiza" entre Usuario y Compra, la cual es N:1, he decidido añadir un atributo de Usuario en la clase Compra, ya que en este caso, el atributo sería necesario, debido a que la cardinalidad mínima es superior a 0*/
@@ -91,13 +97,14 @@ public class Compra {
         this.iddescuento = iddescuento;
     }
 
-    public static Compra nuevaCompra() throws ParseException {
+    public static Compra nuevaCompra()  {
 
         Compra nuevacompra = new Compra();
         Scanner in = new Scanner(System.in);
 
         long idcompra;
-        String metodopagocompra;
+        double preciocompra;
+        String metodopagocompra = "";
 
         boolean repetir;
 
@@ -123,19 +130,69 @@ public class Compra {
         nuevacompra.setId(idcompra);
 
         //En caso de que no se quiera pedir por teclado
-        nuevacompra.setId(Utilidades.numReservas + 1); //Se le suma uno a la cantidad de reservas que existan en utilidades
+        //nuevacompra.setId(Utilidades.numReservas + 1); Se le suma uno a la cantidad de reservas que existan en utilidades
 
         
         System.out.println("");
         
-        /*
-        do {
+        
+        do {//Pedira un precio(double) el cual no podra ser menor igual que 0 o este se volvera a pedir hasta que sea mayor que cero
+
+            System.out.print("¿Cual es el precio de su compra?(La separacion de decimales va con comas): ");//Se pide el precio
+            preciocompra = in.nextDouble();
+
+            System.out.print("¿" + preciocompra + " es su precio de la compra?(true en caso afirmativo y false en negativo): ");//Se le dice al usuario lo que ha introducido y le pregunta si esta bien
+            repetir = in.nextBoolean();
+
+            if (repetir == false) {
+                System.out.println("Vale,entonces introduzcalo de nuevo");//En caso negativo vuelve a preguntar
+                continue;
+            }
+
+            if ((preciocompra <= 00.00)) {
+                System.out.println("El precio introducido no es valido ya que " + preciocompra + " es menor que 00.01,introduzcalo de nuevo"); //En caso de que este sea menor igual que cero se mostrara un mensaje para advertir de que es erroneo.
+            }
+
+        } while ((preciocompra <= 00.00) || (repetir == false));
+        nuevacompra.setPrecio(preciocompra);
+        
+     
+        System.out.println("");
+
+        
+        do {//Se repetira mientras el usuario diga que lo que introducio no es correcto
+
             in = new Scanner(System.in);
 
-            System.out.print("¿Cual es su metodo de pago?"); //Se pregunta por el metodo de pago
+            System.out.print("¿Cual es su metodo de pago?\n1:Tarjeta de credito  2:Contrarembolso  3:PayPal  4:Bizum  5:Otra\n(1,2,3,4 o 5): "); //Se dicen los metodos de pagos
+            int eleccion = in.nextInt();//Se pide la eleccion
+            switch (eleccion) {
+                case 1:
+                    metodopagocompra = "Tarjeta de credito";
+                    break;
 
+                case 2:
+                    metodopagocompra = "Contrarembolso";
+                    break;
 
-            System.out.print("¿" + metodopagocompra + " es su metodo de pago?(true en caso afirmativo y false en negativo): ");//
+                case 3:
+                    metodopagocompra = "PayPal";//Los 4 primeros casos se guarda el elegido en la variable de metododepagocompra
+                    break;
+                    
+                case 4:
+                    metodopagocompra = "Bizum";
+                    break;
+                    
+                case 5:
+                    in = new Scanner(System.in);//En el 5 se pide al usuario que pregunte cual es  la otra
+                    
+                    System.out.print("Introduzca cual es la Otra: ");
+                    String otra = in.nextLine();
+                    metodopagocompra = otra;
+                    break;
+            }
+
+            System.out.print("¿" + metodopagocompra + " es su metodo de pago?(true en caso afirmativo y false en negativo): ");//Se pregunta al usuario si esta es su forma de pago
             repetir = in.nextBoolean();
 
             if (repetir == false) {
@@ -144,10 +201,8 @@ public class Compra {
                 continue;
             }
 
-        } while ((repetir == false)); //Si el NIF esta vacio o este tiene mas o menos de 9 caractertes se volvera a pedir al usuario el NIF
-        */
-        
-        
+        } while ((repetir == false)); //Si usuario dice que es falso lo que introducio se hara otra iteracion
+        nuevacompra.setMetodopago(metodopagocompra);
         
         return nuevacompra;
     }
