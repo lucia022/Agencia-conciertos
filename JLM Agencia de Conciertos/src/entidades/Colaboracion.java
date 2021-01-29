5aae3d23fa815c01830f9a2e09a350cce57b5677
lucia <lucia@LAPTOP-U188EK8J>
2021-01-29 18:27:18 +0100

    subo mi parte

--- JLM Agencia de Conciertos/src/entidades/Colaboracion.java
+++ JLM Agencia de Conciertos/src/entidades/Colaboracion.java
@@ -1,11 +1,25 @@
 
 package entidades;
 
+import java.util.ArrayList;
+
 
 public class Colaboracion extends Concierto{
     
+     private ArrayList<Artista> artistas = new ArrayList<Artista>(); /*relación "actuar" N:N entre artista y colaboración.*/
     public Colaboracion (){
         super();
     }
     
+    public Colaboracion(ArrayList<Artista> artistas){
+        this.artistas = artistas;
+    }
+
+    public ArrayList<Artista> getArtistas() {
+        return artistas;
+    }
+
+    public void setArtistas(ArrayList<Artista> artistas) {
+        this.artistas = artistas;
+    }
   }
