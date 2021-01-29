d89b03fe6e6c74205e27abc44f107f0011203723
lucia <lucia@LAPTOP-U188EK8J>
2021-01-29 18:27:53 +0100

    subo mi parte

--- JLM Agencia de Conciertos/src/entidades/Individual.java
+++ JLM Agencia de Conciertos/src/entidades/Individual.java
@@ -3,9 +3,21 @@ package entidades;
 
 
 public class Individual extends entidades.Concierto{
+    private Artista artista; /*relación "actuar" 1:N entre artista e individual, donde añadimos un objeto de la clase artista en la clase individual*/
     
     public Individual (){
         super();
     }
     
+    public Individual(Artista artista){
+        this.artista = artista;
+    }
+
+    public Artista getArtista() {
+        return artista;
+    }
+
+    public void setArtista(Artista artista) {
+        this.artista = artista;
+    }
 }
